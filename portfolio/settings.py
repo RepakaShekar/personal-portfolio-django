c4d3d13e5cd51c59050fe1eaab3d7b5e371ac26c
RepakaShekar <repakashekar3@gmail.com>
2026-02-13 15:58:17 +0530

    Updated requirements for production deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jlh9$h#cf8vclfiu-r%z7$2hx231jp15nwq+4e3pbkpe-2v(s8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
